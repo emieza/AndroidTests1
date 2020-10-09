66203809bb7ddaf5e5a11b388bd27bd20adb8c71
Enric <emieza@xtec.cat>
2020-10-09 16:32:42 +0200

    table view

--- app/src/main/java/com/example/empty1/RankActivity.java
+++ app/src/main/java/com/example/empty1/RankActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -55,17 +56,41 @@ public class RankActivity extends AppCompatActivity {
             rec.temps = 0;
             records.add( rec );
 
-            String newtext = text + nom + ": " + Integer.toString(intents) + " intents\n";
-            text = newtext;
+            //String newtext = text + nom + ": " + Integer.toString(intents) + " intents\n";
+            //text = newtext;
         }
         // mostrem ranking en taula
         TableLayout tableRank = findViewById(R.id.tableRank);
+        TableRow.LayoutParams  params1=new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT,1.0f);
+        TableRow.LayoutParams params2=new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,TableRow.LayoutParams.WRAP_CONTENT);
+
+        for( Record record: records ) {
+            TableRow row = new TableRow(this);
+            TextView tv1 = new TextView(this);
+            TextView tv2 = new TextView(this);
+            TextView tv3 = new TextView(this);
+            // set text
+            tv1.setText(record.nom);
+            tv2.setText(Integer.toString(record.intents));
+            tv3.setText(Double.toString(record.temps));
+            // layout params
+            tv1.setLayoutParams(params1);
+            tv2.setLayoutParams(params1);
+            tv3.setLayoutParams(params1);
+            // afegim a la row i al table layout
+            row.addView(tv1);
+            row.addView(tv2);
+            row.addView(tv3);
+            row.setLayoutParams(params2);
+            tableRank.addView(row);
+        }
+
 
         //tableRank.
 
         // mostrem ranking en textView
-        TextView tv = findViewById(R.id.rankText);
-        tv.setText(text);
+        //TextView tv = findViewById(R.id.rankText);
+        //tv.setText(text);
         //Log.v("DEBUG", text);
     }
 }
